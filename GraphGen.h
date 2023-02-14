ca0bbfa02ce9b4d6bc28b505891eddb288b03d5b
qqwerty741 <58818436+qqwerty741@users.noreply.github.com>
2023-02-14 17:23:47 +0300

    UPD
    Delete regular-graph generation, fix some bugs

--- GraphGen.h
+++ GraphGen.h
@@ -4,15 +4,17 @@
 #include <random>
 #include <chrono>
 #include<algorithm>
+#include<iostream>
 
 
 struct Graph
 {
-	int seed;
+	int seed, e, v;
 	std::vector<std::vector<int>> mtx;
+	std::vector<std::pair<int, int>> edges;
 };
 
-auto genTree(int minG = 0, int maxG = 1e7, int seed = std::chrono::steady_clock::now().time_since_epoch().count()/*, std::string add = ""*/) {
+auto genTree(int minG = 0, int maxG = 1e7, int seed = std::chrono::steady_clock::now().time_since_epoch().count()) {
 	std::mt19937 a(seed);
 	long long v = a() % (maxG - minG + 1) + minG;
 	std::vector<int> ans;
@@ -23,70 +25,35 @@ auto genTree(int minG = 0, int maxG = 1e7, int seed = std::chrono::steady_clock:
 	for (unsigned long long i = 1; i < v; i++) 
 		ans.push_back(a() % i);
 	for (unsigned long long i = 0; i < ans.size(); i++)
-		if (ans[i] != -1)
+		if (ans[i] != -1) {
 			ans2[i][ans[i]] = ans2[ans[i]][i] = 1;
+			Ans.edges.emplace_back(i, ans[i]);
+		}
 	Ans.mtx = ans2;
-	return Ans;
-
-}
-auto genRegul(int minG = 0, int maxG = 1e7, int minS = 0, int maxS = 1e5, int seed = std::chrono::steady_clock::now().time_since_epoch().count()) { //NotWorked
-	std::mt19937 a(seed);
-	long long v = a() % (maxG - minG + 1) + minG;
-	long long d = a() % (maxS - minS + 1) + minS;
-	std::vector<int> g(v);
-	std::vector<std::vector<int>> ans(v, std::vector<int>(v, 0));
-	for (int i = 0; i < d - d % 2; i++) {
-		std::vector<int> g(v);
-		for (int i = 0; i < v; i++)
-			g[i] = i;
-		for (int i = 0; i < v; i++) {
-			int z;
-			int r = a() % v;
-			z = g[i];
-			g[i] = g[r];
-			g[r] = z;
-		}
-		while (g.size() >= 2) {
-			int rs = g[g.size() - 1], re = g[g.size() - 2];
-			if (ans[rs][re] == 0) {
-				g.pop_back();
-				g.pop_back();
-				ans[rs][re] = 1;
-				ans[re][rs] = 1;
-			}
-			else {
-				for (int i = 0; i < g.size(); i++)
-				{
-					int z;
-					int r = a() % g.size();
-					z = g[i];
-					g[i] = g[r];
-					g[r] = z;
-				}
-			}
-		}
-	}
-	Graph Ans;
-	Ans.seed = seed;
-	Ans.mtx = ans;
+	Ans.v = v;
+	Ans.e = Ans.edges.size();
 	return Ans;
 }
 
 auto genGraph(int minG, int maxG = 1e9, int minS = 0, int maxS = 1e5, int seed = std::chrono::steady_clock::now().time_since_epoch().count()) {
 	std::mt19937 a(seed);
 	long long v = a() % (maxG - minG + 1) + minG;
-	maxS = (maxS > (v * (v - 1)) / 2 ? (v * (v - 1)) / 2 : v);
+	maxS = (maxS > (v * (v - 1)) / 2 ? (v * (v - 1)) / 2 : maxS);
 	long long d = a() % (maxS - minS + 1) + minS;
 	std::vector<std::vector<int>> ans(v, std::vector<int>(v, 0));
+	Graph Ans;
 	for (int i = 0; i < d; i++) {
 		int q = a() % v, w = a() % v;
 		if (q == w)i--;
 		else if (ans[q][w] == 1)i--;
-		else ans[q][w] = ans[w][q] = 1;
-		
+		else {
+			ans[q][w] = ans[w][q] = 1;
+			Ans.edges.emplace_back(q, w);
+		}
 	}
-	Graph Ans;
 	Ans.seed = seed;
 	Ans.mtx = ans;
+	Ans.v = v;
+	Ans.e = Ans.edges.size();
 	return Ans;
 }
