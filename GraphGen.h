15f8e555827926401592c326e8ba62993a845864
qqwerty741 <58818436+qqwerty741@users.noreply.github.com>
2023-02-15 21:37:48 +0300

    UPD

--- GraphGen.h
+++ GraphGen.h
@@ -99,11 +99,22 @@ auto genNull(int minG = 0, int maxG = 1e9, int seed = std::chrono::steady_clock:
 auto genCompl(int minG = 0, int maxG = 1e9, int seed = std::chrono::steady_clock::now().time_since_epoch().count()) {
 	std::mt19937 a(seed);
 	long long v = a() % (maxG - minG + 1) + minG;
-	std::vector<std::vector<int>> ans(v, std::vector<int>(v, 1));
-	for (int i = 0; i < v; i++)ans[i][i] = 0;
 	Graph Ans;
+	std::vector<std::vector<int>> g(v, std::vector<int>(v));
+	Ans.listsm.resize(v);
+	for (int i = 1; i < v; i++) {
+		for (int j = 0; j < i; j++) {
+			g[i][j] = a() % 2;
+			g[j][i] = g[i][j];
+			if (g[i][j] == 1) {
+				Ans.listsm[i].push_back(j);
+				Ans.listsm[j].push_back(i);
+				Ans.edges.emplace_back(i, j);
+			}
+		}
+	}
 	Ans.seed = seed;
-	Ans.mtx = ans;
+	Ans.mtx = g;
 	Ans.v = v;
 	Ans.e = Ans.edges.size();
 	return Ans;
@@ -151,24 +162,6 @@ auto genPseudo(int minG = 0, int maxG = 1e9, int minS = 0, int maxS = 1e5, int s
 	return Ans;
 }
 
-//RegularGraph - Граф с одинаковыми степенями вершин
-//		Ген: Рандомное число до количества вершин
-//CyclicGraph - граф-цикл
-//		Ген: Случайное дерево с дополнительным ребром
-
-/*
-	for (int i = 0; i < v; i++) {
-			int z;
-			int r = a() % v;
-			z = g[i];
-			g[i] = g[r];
-			g[r] = z;
-		}
-*/
-//long long v = a() % (maxG - minG + 1) + minG;
-//от minG до maxG
-
-
 auto genBipart(int minG = 0, int maxG = 1e9, int seed = std::chrono::steady_clock::now().time_since_epoch().count()){
 	std::mt19937 a(seed);
 	long long v = a() % (maxG - minG + 1) + minG;
@@ -191,6 +184,8 @@ auto genBipart(int minG = 0, int maxG = 1e9, int seed = std::chrono::steady_cloc
 				if (g[i][j] == 1) {
 					Ans.listsm[i].push_back(j);
 					Ans.listsm[j].push_back(i);
+					Ans.edges.emplace_back(i, j);
+
 				}
 			}
 		}
@@ -199,5 +194,45 @@ auto genBipart(int minG = 0, int maxG = 1e9, int seed = std::chrono::steady_cloc
 	Ans.mtx = g;
 	Ans.v = v;
 	Ans.e = e;
+	return Ans;
+}
+
+auto genCyclic(int minG = 0, int maxG = 1e7, int seed = std::chrono::steady_clock::now().time_since_epoch().count()) {
+	std::mt19937 a(seed);
+	long long v = a() % (maxG - minG + 1) + minG;
+	std::vector<int> ans;
+	ans.push_back(-1);
+	std::vector<std::vector<int>> ans2(v, std::vector<int>(v, 0));
+	Graph Ans;
+	Ans.seed = seed;
+	for (unsigned long long i = 1; i < v; i++)
+		ans.push_back(a() % i);
+	for (unsigned long long i = 0; i < ans.size(); i++)
+		if (ans[i] != -1) {
+			ans2[i][ans[i]] = ans2[ans[i]][i] = 1;
+			Ans.edges.emplace_back(i, ans[i]);
+		}
+	std::vector<int> r(v, 0);
+	for (int i = 0; i < v; i++)
+		r[i] = i;
+	for (int i = 0; i < v; i++) {
+		int z;
+		int zf = a() % v;
+		z = r[i];
+		r[i] = r[zf];
+		r[zf] = z;
+	}
+	int fl = 0;
+	for(int i = 1; i < v; i++)
+		if(!ans2[r[0]][r[i]]){
+			ans2[r[0]][r[i]] = ans2[r[i]][r[0]] = 1;
+			fl = 1;
+			break;
+		}
+	if (!fl)
+		ans2[r[1]][r[2]] = ans2[r[2]][r[1]] = 1;
+	Ans.mtx = ans2;
+	Ans.v = v;
+	Ans.e = Ans.edges.size();
 	return Ans;
 }
